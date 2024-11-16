25cb84280b79db705be56db2474ad881630701ef
DOJapp <satender@dojapp.com>
2024-11-16 16:51:03 +0530

    change corse

--- src/app.js
+++ src/app.js
@@ -1,54 +1,59 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import userRouter from './routes/users/index.routes.js'; 
-import adminRouter from './routes/admin/index.routes.js'; 
-import storeRouter from './routes/store/index.routes.js'; 
-import logger from './utils/logger.js';  
-import helmet from "helmet"; 
+import userRouter from "./routes/users/index.routes.js";
+import adminRouter from "./routes/admin/index.routes.js";
+import storeRouter from "./routes/store/index.routes.js";
+import logger from "./utils/logger.js";
+import helmet from "helmet";
 
 const app = express();
 
 app.use(helmet());
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }));
+
+app.use(
+  cors({
+    origin: "https://frontend-one-navy-20.vercel.app/",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // app.use(express.json({ limit: "16kb" }));
 // app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-
-
 // Request logging middleware
 app.use((req, res, next) => {
-    logger.info(`${req.method} ${req.url}`);
-    next();
+  logger.info(`${req.method} ${req.url}`);
+  next();
 });
 
 // Error logging middleware
 app.use((err, req, res, next) => {
-    logger.error(`${err.message} - ${req.method} ${req.url}`);
-    logger.error(err.stack);
+  logger.error(`${err.message} - ${req.method} ${req.url}`);
+  logger.error(err.stack);
 
-    res.status(err.status || 500).json({
-        message: err.message || 'Internal Server Error',
-        status: err.status || 500,
-    });
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+    status: err.status || 500,
+  });
 });
 
 // Mount the user routes at /api/v1
 app.use("/api/v1/users", userRouter);
 
-
 // Mount the user routes at /api/v1
 app.use("/api/v1/store", storeRouter);
-
 
 // Mount the user routes at /api/v1
 app.use("/api/v1/admin", adminRouter);
